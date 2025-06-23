abb3c245596ecc864ef717f868c024fb365f49b4
W0NYV <w0nyv.707@gmail.com>
2025-06-24 02:23:14 +0900

    色緑

--- shaders/out.glsl
+++ shaders/out.glsl
@@ -12,7 +12,7 @@ void main() {
     vec2 p = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
     
     vec4 col = texture(feedbackTex, uv);// * (sliders[0] * 0.5 + 1.0);
-    col.rgb = sliders[6] == 1.0 ? mix(vec3(0.0), vec3(1.0, 0.45, 0.0), col.rgb) : col.rgb;
+    col.rgb = sliders[6] == 1.0 ? mix(vec3(0.0), vec3(0.45, 0.95, 0.3), col.rgb) : col.rgb;
 
     color = col * (1.0 - buttons[31].x);
 }
